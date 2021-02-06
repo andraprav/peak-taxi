f7b2931e0a4dcc540bd30ab15ee25a23fee89525
andraprav <andraprav@gmail.com>
2021-02-06 15:09:18 +0200

    add trip_id

--- src/main/scala/com/peak/taxi/Main.scala
+++ src/main/scala/com/peak/taxi/Main.scala
@@ -4,6 +4,7 @@ import org.apache.spark.sql.{DataFrame, SparkSession, functions}
 import org.apache.spark.sql.functions.{col, date_format, hour, lit, monotonically_increasing_id}
 
 import java.io.File
+import scala.reflect.internal.util.TableDef.Column
 
 object Main {
 
@@ -21,12 +22,11 @@ object Main {
     val parquetFileDFPickUp = getDataFrame(spark, listOfFiles.head.toString,
       "pickup_datetime", "pickup_taxizone_id")
 
+    println(parquetFileDFDropOff.columns.mkString("Array(", ", ", ")"))
+
     val parquetFileDF = parquetFileDFDropOff
       .union(parquetFileDFPickUp)
-      .select(functions.concat(
-        col("date"),
-        lit(","),
-        col("taxizone_id")).as("date_taxizone"))
+      .select(getConcatColumn, col("trip_id"))
 
     println(parquetFileDF.columns.mkString("Array(", ", ", ")"))
 
@@ -34,6 +34,13 @@ object Main {
     println(header.mkString("Array(", ", ", ")"))
 
     spark.stop()
+  }
+
+  private def getConcatColumn = {
+    functions.concat(
+      col("date"),
+      lit(","),
+      col("taxizone_id")).as("date_taxizone")
   }
 
   def initSpark = {
@@ -46,7 +53,7 @@ object Main {
 
   private def getDataFrame(spark: SparkSession, file: String, dateTimeColumn: String, taxizoneColumn: String): DataFrame = {
     spark.read.parquet(file)
-      .select(dateTimeColumn, taxizoneColumn)
+      .select(dateTimeColumn, taxizoneColumn, "trip_id")
       .withColumn(dateTimeColumn, date_format(col(dateTimeColumn), "yyyy-MM-dd HH"))
       .withColumnRenamed(dateTimeColumn, "date")
       .withColumnRenamed(taxizoneColumn, "taxizone_id")
